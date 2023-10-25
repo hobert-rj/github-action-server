55fffc4cf6267d4dc2725fc97d1156b132954350
hosseinrajabi <hosseinrajabi125@gmail.com>
2023-10-25 18:58:37 +0330

    send logs as response

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { exec } = require('child_process');
+const {exec} = require('child_process');
 const minimist = require('minimist');
 
 const app = express();
@@ -11,26 +11,32 @@ const projectDirectory = args.directory;
 app.get('/deploy-pwa-stage', (req, res) => {
     process.chdir(projectDirectory);
 
+    let stdoutData = '';
+    let stderrData = '';
+
     const npmScript = exec('yarn run deploy:pwa:stage');
 
     npmScript.stdout.on('data', (data) => {
         console.log(data);
+        stdoutData += data + '\n';
     });
 
     npmScript.stderr.on('data', (data) => {
-        console.log(data);
+        console.error(data);
+        stderrData += data + '\n';
     });
 
     npmScript.on('exit', (code) => {
         if (code === 0) {
             console.log('Script executed successfully');
-            res.status(200).send('Script executed successfully');
+            res.status(200).send(stdoutData + '\n' + 'Script executed successfully');
         } else {
             console.error(`Script execution failed with code ${code}`);
-            res.status(500).send('Error running the script');
+            res.status(500).send(stderrData + '\n' + 'Error running the script');
         }
     });
 });
+
 
 app.listen(port, () => {
     console.log(`Node server is running on port ${port}`);
